4197c961ac5dfda2f93c0dc9d5b55d5d5ed0f3d5
bosco227 <antoniobosco2612@gmail.com>
2026-05-03 11:55:51 -0300

    Tirada a integração com postgres

--- src/controllers/agendamentoController.js
+++ src/controllers/agendamentoController.js
@@ -1,51 +1,60 @@
-const pool = require("../db");
+let agendamentos = [];
 
-exports.criarAgendamento = async (req, res) => {
+exports.criarAgendamento = (req, res) => {
   const { nome, data, hora } = req.body;
 
-  try {
-    const result = await pool.query(
-      "INSERT INTO agendamentos (nome, data, hora) VALUES ($1, $2, $3) RETURNING *",
-      [nome, data, hora],
-    );
-
-    res.status(201).json(result.rows[0]);
-  } catch (err) {
-    // 👇 trata conflito do banco
-    if (err.code === "23505") {
-      return res.status(400).json({
-        erro: "Já existe um agendamento para esse horário",
-      });
-    }
-
-    console.error(err);
-    res.status(500).json({ erro: "Erro no servidor" });
+  if (!nome || !data || !hora) {
+    return res.status(400).json({ erro: "Dados obrigatórios" });
   }
+
+  const hoje = new Date();
+  hoje.setHours(0, 0, 0, 0);
+
+  const dataAgendada = new Date(data);
+  dataAgendada.setHours(0, 0, 0, 0);
+
+  const diferenca = (dataAgendada - hoje) / (1000 * 60 * 60 * 24);
+
+  if (diferenca < 3) {
+    return res.status(400).json({
+      erro: "Agendamento deve ter no mínimo 3 dias de antecedência",
+    });
+  }
+
+  const conflito = agendamentos.find((a) => a.data === data && a.hora === hora);
+
+  if (conflito) {
+    return res.status(400).json({
+      erro: "Já existe um agendamento para esse horário",
+    });
+  }
+
+  const novo = {
+    id: Date.now(),
+    nome,
+    data,
+    hora,
+  };
+
+  agendamentos.push(novo);
+
+  res.status(201).json(novo);
 };
 
-exports.listarAgendamentos = async (req, res) => {
-  const result = await pool.query("SELECT * FROM agendamentos");
-  res.json(result.rows);
+exports.listarAgendamentos = (req, res) => {
+  res.json(agendamentos);
 };
 
-exports.deletarAgendamento = async (req, res) => {
+exports.deletarAgendamento = (req, res) => {
   const { id } = req.params;
 
-  try {
-    const result = await pool.query(
-      "DELETE FROM agendamentos WHERE id = $1 RETURNING *",
-      [id],
-    );
+  const index = agendamentos.findIndex((a) => a.id == id);
 
-    if (result.rowCount === 0) {
-      return res.status(404).json({
-        erro: "Agendamento não encontrado",
-      });
-    }
-
-    res.json({ mensagem: "Agendamento deletado com sucesso" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ erro: "Erro no servidor" });
+  if (index === -1) {
+    return res.status(404).json({ erro: "Agendamento não encontrado" });
   }
+
+  agendamentos.splice(index, 1);
+
+  res.json({ mensagem: "Deletado com sucesso" });
 };
